d2d16033b907537b3ae653222043cb71c136ec64
Mihai <48624094+mihaichris@users.noreply.github.com>
2024-01-08 08:06:55 +0200

    added combination method for wardrobe sizes

--- src/main/kotlin/Wardrobe.kt
+++ src/main/kotlin/Wardrobe.kt
@@ -9,6 +9,27 @@ class Wardrobe(private var elements: MutableList<Element>) {
         elements.add(element)
     }
 
+    fun findWardrobeCombinations(wardrobeSizes: List<Int>): List<List<Element>> {
+        val combinations = mutableListOf<List<Element>>()
+
+        fun generateCombinations(currCombination: List<Element>, index: Int, sum: Int) {
+            if (sum == WALL_LENGTH) {
+                combinations.add(currCombination)
+                return
+            }
+            if (sum > WALL_LENGTH || index == wardrobeSizes.size) {
+                return
+            }
+            generateCombinations(currCombination, index + 1, sum)
+            val newCombination = currCombination.toMutableList()
+            newCombination.add(Element(wardrobeSizes[index]))
+            generateCombinations(newCombination, index, sum + wardrobeSizes[index])
+        }
+
+        generateCombinations(emptyList(), 0, 0)
+        return combinations
+    }
+
     fun getElements(): List<Element> {
         return elements.toList()
     }
